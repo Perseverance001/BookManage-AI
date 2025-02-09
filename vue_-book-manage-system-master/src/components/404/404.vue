863dc04fb42c7aa7da6950cf46d794858026b53b
Perseverance001 <1817117243@qq.com>
2025-02-09 11:39:17 +0800

    initial...

---
+++ vue_-book-manage-system-master/src/components/404/404.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+</script>
+<style>
+.container {
+    width: 100%;
+    height: 100vh;
+    background: url(https://4kwallpapers.com/images/walls/thumbs/18308.jpg) no-repeat;
+    background-size: cover;
+}
+</style>
